9b043a573d5c40a94c0060b4ea81a148e73fe2ab
DSDHX <56496118+DSDHX@users.noreply.github.com>
2025-02-01 00:02:26 -0800

    Assignment 4

--- Pix/Pix/CommandDictionary.cpp
+++ Pix/Pix/CommandDictionary.cpp
@@ -14,6 +14,8 @@
 #include "CmdSetViewport.h"
 #include "CmdSetClipping.h"
 
+//#include "MathHelper.h"
+
 CommandDictionary* CommandDictionary::Get()
 {
 	static CommandDictionary sInstance;
@@ -44,6 +46,12 @@ CommandDictionary::CommandDictionary()
 	RegisterCommand<CmdShowViewport>();
 	RegisterCommand<CmdSetViewport>();
 	RegisterCommand<CmdSetClipping>();
+
+	//Matrix4 rotX = Matrix4::RotationX((45.0f * 3.141592f) / 180.0f);
+	//Matrix4 trans = Matrix4::Translation(10.0f, 5.0f, 3.0f);
+	//Matrix4 final = rotX * trans;
+	//Matrix4 invFinal = MathHelper::Inverse(final);
+	//Matrix4 identity = final * invFinal;
 }
 
 TextEditor::LanguageDefinition CommandDictionary::GenerateLanguageDefinition()
